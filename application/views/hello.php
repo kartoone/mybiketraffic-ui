6eb7b198943ae05f3526fc188b045fd919dcfb94
RaynierDiaz <radbim1.0@gmail.com>
2022-03-15 19:42:01 -0500

    Test commit

--- application/views/hello.php
+++ application/views/hello.php
@@ -3,6 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <div id="container">
 	<h1>Hello Codeigniter!!!</h1>
+	<h1>Hello Codeigniter!!!</h1>
+	<h1>Hello Codeigniter!!!</h1>
+	<h1>Hello Codeigniter!!!</h1>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
