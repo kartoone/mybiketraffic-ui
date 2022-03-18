1e15a313083f4e2f74963b063264a81948583e5d
Brian Toone <brtoone@samford.edu>
2022-03-18 18:43:08 -0500

    test rides page with test data

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,7 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends BT_Controller {
 
-	/**
+    public function __construct()
+    {
+        parent::__construct();
+	}
+ 	/**
 	 * Index Page for this controller.
 	 *
 	 * Maps to the following URL
@@ -25,22 +29,37 @@ class Welcome extends BT_Controller {
 		$this->load->view('templates/footer');
 	}
 
+	// http://localhost/simple/welcome/hello 
 	public function hello()
 	{
 		$this->load->view('templates/header');
 		$this->load->view('hello');
 		$this->load->view('templates/footer');
 	}
+
+	// http://localhost/simple/welcome/privacy 
 	public function privacy()
 	{
 		$this->load->view('templates/header');
 		$this->load->view('privacy');
 		$this->load->view('templates/footer');
 	}
+
+	// http://localhost/simple/welcome/credits 
 	public function credits()
 	{
 		$this->load->view('templates/header');
 		$this->load->view('credits');
 		$this->load->view('templates/footer');
 	}
+
+	// http://localhost/simple/welcome/rides 
+	public function rides() {
+		$this->load->view('templates/header');
+		$this->data['rides'] = unserialize(file_get_contents('ridelist.txt'));
+//		ddebug($this->data['rides']);
+		$this->load->view('rides',$this->data);
+		$this->load->view('templates/footer');
+	}
+
 }
---
+++ application/views/rides.php
@@ -0,0 +1,91 @@
+<style>
+form {
+	display:inline;
+}
+</style>
+<h3 style="display:inline">Your rides</h3>
+<div style="display:inline;margin-left:10px;vertical-align:text-bottom"><?= anchor('rides/import','IMPORT NEW RIDES'); ?></div>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">  
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+<?php 
+	echo form_open('rides/multisubmit',  ['id'=>'multiform']);
+?>
+
+<table id="ridetable" class="table table-sm compact">
+  <thead class="thead-light">
+      <tr>
+        <th>&nbsp;</th>
+        <th>&nbsp;</th>
+        <th colspan="3">VEHICLE STATS</th>
+        <th colspan="6" style="border-left:1px solid #000">RIDE STATS (while radar enabled)</th>
+        <th>&nbsp;</th>
+      </tr>
+      <tr>
+        <th><?php echo form_checkbox('checkallmulti', 1, false, 'onclick="$(\'.checkall\').prop(\'checked\', $(this).prop(\'checked\'));"'); ?></th>
+		<th>Datetime (TZ)</th>
+        <th>Total</th>
+        <th class="imperial">per mile</th>
+        <th class="metric">per km</th>
+        <th>per minute</th>
+        <th class="imperial" style="border-left:1px solid #000">Miles</th>
+        <th class="metric" style="border-left:1px solid #000">Kms</th>
+        <th class="imperial">Disabled <br />(miles)</th>
+        <th class="metric">Disabled <br />(kms)</th>
+        <th>Moving time<br />(minutes)</th>
+        <th>Disabled <br />(minutes)</th>
+        <th class="imperial">Avg speed<br />(mph)</th>
+		<th class="metric">Avg speed<br />(kph)</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php foreach ($rides as $ride): ?>
+<?php 
+    $videostr = '[ ';
+    $i=0;
+    if ($ride->videos) {
+      $vid = end($ride->videos);
+      $videostr .= anchor("uploads/{$ride->user_id}/{$ride->id}.sm/{$vid->virbvideo}.mp4", "video", ['target'=>'_blank']) . ' ';
+    }
+    $videostr .= ']';
+?>
+<tr>
+  <td><?php echo form_checkbox(['name'=>'checkall[]','value'=>$ride->id,'checked'=>false,'class'=>'checkall']); ?></td>
+  <td><span class="moving"><?php echo anchor('rides/view/'.$ride->id, $ride->localtimestr); ?></span> <?php echo $videostr; ?></td>
+  <td></span><span class="moving"><?php echo $ride->movingcars; ?></span></td>
+  <td class="imperial"></span><span class="moving"><?php echo $ride->movingcpmile; ?></span></td>
+  <td class="metric"><span class="moving"><?php echo $ride->movingcpkm; ?></span></td>
+  <td><span class="moving"><?php echo $ride->movingcpminute; ?></span></td>
+  <td class="imperial" style="border-left:1px solid #000"><?php echo $ride->diststr; ?></td>
+  <td class="metric" style="border-left:1px solid #000"><?php echo $ride->diststrkm; ?></td>
+  <td class="imperial"><?php echo $ride->disableddist; ?></td>
+  <td class="metric"><?php echo $ride->disableddistkms; ?></td>
+  <td><?php echo $ride->movingstr; ?></td>
+  <td><?php echo $ride->disabledstr; ?></td>
+  <td class="imperial"><?php echo $ride->speedstr; ?></td>
+  <td class="metric"><?php echo $ride->speedstrkph; ?></td>
+  <td>
+      <?php
+            echo anchor("rides/delete/{$ride->id}", img("assets/img/redx.gif") . '&nbsp;', ['title'=>'delete ride','onclick'=>"return confirm('Are you sure you want to delete this ride?');"]);
+            echo ' | ';
+            echo anchor("rides/attachvideo/{$ride->id}", "&nbsp;&#128249;", ['title'=>'attach video']);
+            echo ' | ';
+            echo anchor("rides/refreshcars/{$ride->id}", "&nbsp;&#128663;", ['title'=>'refresh car count']);
+            echo ' | ';
+            echo anchor("rides/segmentor/{$ride->id}", "&sect;", ['title'=>'create segment']);
+            if ($ride->private == 1) {
+                echo ' | ';
+                echo anchor("rides/makepublic/{$ride->id}", "&nbsp;&#128274;", ['title'=>'make ride public']);
+            } else {
+                echo ' | ';
+                echo anchor("rides/makeprivate/{$ride->id}", "&nbsp;&#128275;", ['title'=>'make ride private']);
+            }
+      ?>
+  </td>
+</tr>
+<?php endforeach; ?>
+</tbody>
+</table>
+<?php
+	echo form_close();
+?>
