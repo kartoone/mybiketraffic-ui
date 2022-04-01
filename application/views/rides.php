71e3c04d1601a171d34cd1814c1abf2d65de79ba
RaynierDiaz <radbim1.0@gmail.com>
2022-04-01 17:45:25 -0500

    latest week of 4-1-2022

--- application/views/rides.php
+++ application/views/rides.php
@@ -1,27 +1,29 @@
 <style>
 form {
   display:inline;
-  color: red;
-  background-color: red;
+  color: white;
+  background-color: yellow;
 }
 </style>
 <style>
 body {
-  color: lightgray;
-  background-color: lightgreen;
+  color: yellow;
+  background-color: lightgreen; 
 }
 main{
-	background-color:yellow;
+	color: black;
+	background-color:white;
 }
 h1 {
-  color: green;
+  color: black;
 }
 .scrollable {
   overflow-y: scroll;
 }
 </style>
-<h1 style="display:inline">Your rides</h1>
-<div style="display:inline;margin-left:10px;vertical-align:text-bottom"><?= anchor('rides/import','IMPORT NEW RIDES'); ?></div>
+<h1 style="display:inline">Your Rides</h1>
+<button>Share rides</button>
+<div class="btn btn-default" style="display:inline;margin-left:10px;vertical-align:text-bottom"><?= anchor('rides/import','IMPORT NEW RIDES'); ?></div>
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">  
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
 <div class="container-fluid">
@@ -29,30 +31,47 @@ h1 {
 		<div class="col-md-6 d-flex flex-column">
 			<div class="row">
 				<div class="col-md-12">
-					<div style="height:360px;background-color:white">
+					<div style="height:700px;">
 						<div class="row">
-							<div class="col-md-6">
-								BIKE STATS	
+							<div class="col-md-6 center" style="border-radius: 12px;background: linear-gradient(315deg, #cacaca, #f0f0f0);box-shadow:  -8px -8px 10px #bebebe,8px 8px 10px #ffffff;">
+								BIKE STATS	<br>
+								subhead <br>
+								Bike Icon image
 							</div>
-							<div class="col-md-6">
-								VEHICLE STATS	
+							<div class="col-md-6 center"style="border-radius: 12px;background: linear-gradient(315deg, #cacaca, #f0f0f0);box-shadow:  -8px -8px 10px #bebebe,8px 8px 10px #ffffff;">
+								VEHICLE STATS <br>
+								subhead <br>
+								Vehicle Icon image
+									<div>
+									<i class="fa-solid fa-bicycle"></i>
+									</div>
+							</div>
+							<div class="col-md-6 center"style="height:600px;border-radius: 30px;background: linear-gradient(315deg, #cacaca, #f0f0f0);box-shadow:  -8px -8px 8px #bebebe,8px 8px 8px #ffffff;">
+								average speed:     13.62 <br>
+								total miles:       45
+							</div>
+							<div class="col-md-6 center"style="height:600px;border-radius: 30px;background: linear-gradient(315deg, #cacaca, #f0f0f0);box-shadow:  -8px -8px 8px #bebebe,8px 8px 8px #ffffff;">
+								per min:     0.7 <br>
+								per miles:   3.3 <br>
+								total:       65
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div class="row scrollable flex-grow-1" style="height:600px">
+			<div class="row scrollable flex-grow-1" style="height:400px;border-radius: 30px;background: linear-gradient(315deg, #cacaca, #f0f0f0);box-shadow:  -8px -8px 16px #bebebe,8px 8px 16px #ffffff;">
 				<div class="col-md-12">
 					<?php 
 						echo form_open('rides/multisubmit',  ['id'=>'multiform']);
 					?>
 					<table id="ridetable" class="table table-md compact">
-					  <thead class="thead-dark">
+					  <thead class="">
 					  	<tr>
 					       	<th><?php echo form_checkbox('checkallmulti', 1, false, 'onclick="$(\'.checkall\').prop(\'checked\', $(this).prop(\'checked\'));"'); ?></th>
 							<th>Datetime (TZ)</th>
 					       	<th>#cars</th>
 					        <th>miles</th>
+							<th><button>Share rides</button></th>
 					    </tr>
 					  </thead>
 					  <tbody>
@@ -70,7 +89,7 @@ h1 {
 						  <td><?php echo form_checkbox(['name'=>'checkall[]','value'=>$ride->id,'checked'=>false,'class'=>'checkall']); ?></td>
 						  <td><span class="moving"><?php echo anchor('rides/view/'.$ride->id, $ride->localtimestr); ?></span> <?php echo $videostr; ?></td>
 						  <td><span class="moving"><?php echo $ride->movingcars; ?></span></td>
-						  <td class="imperial" style="border-left:1px solid #000"><?php echo $ride->diststr; ?></td>
+						  <td class="imperial" style="border-left:2px solid #000"><?php echo $ride->diststr; ?></td>
 						</tr>
 						<?php endforeach; ?>
 						</tbody>
@@ -81,8 +100,10 @@ h1 {
 				</div>
 			</div>
 		</div>
-		<div class="col-md-6">
-			MAP<br clear="all" />
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26629.495750413756!2d-86.82588801001114!3d33.45746111760029!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8889193f37ce0795%3A0xe74c32794bbfc294!2sHomewood%2C%20AL!5e0!3m2!1sen!2sus!4v1648786754729!5m2!1sen!2sus" width="700" height="1100" style="border-radius: 30px;background: linear-gradient(315deg, #cacaca, #f0f0f0);box-shadow:  -8px -8px 16px #bebebe,14px 14px 28px #ffffff;;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 		</div>
+			</div>
 	</div>
 </div>
+
+
